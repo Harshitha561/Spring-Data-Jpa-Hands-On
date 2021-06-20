7d6a549e24ca5ea67c1e01fa40f11ca38fea2470
digaj <digaj@192.168.100.5>
2021-06-20 22:36:24 +0530

    SpringDataJPA Hands-on

---
+++ orm-learn/src/main/java/com/cognizant/ormlearn/service/CountryService.java
@@ -0,0 +1,25 @@
+package com.cognizant.ormlearn.service;
+
+import java.util.List;
+
+import com.cognizant.springlearn.service.exception.CountryNotFoundException;
+import com.cognizant.ormlearn.model.Country;
+
+public interface CountryService {
+
+	public List<Country> getAllCountries();
+
+	public Country findCountryByCode(String countryCode) throws CountryNotFoundException;
+
+	public void addCountry(Country country);
+
+//   public Country updateCountry(String code, String name);
+
+	void deleteCountry(String code);
+	
+	List<Country> findCountryByCharacter(String name);
+
+	List<Country> findCountryUsingSingleCharacter(String name);
+
+	
+}
